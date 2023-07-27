cf3b12db1ec4b3d0a6ab14ffcdde2912accb5227
NyaShinn1204 <team.nyarara@gmail.com>
2023-07-27 18:27:27 +0900

    Update main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import subprocess
+import tkinter as tk
+import customtkinter as ctk
+
+def get_hwid():
+    cmd = 'wmic csproduct get uuid'
+    uuid = str(subprocess.check_output(cmd))
+    pos1 = uuid.find("\\n")+2
+    uuid = uuid[pos1:-15]
+    return uuid
+
+print(f"""          
+       &#BB#&       
+     B?^:::^~?B        _______             _____      _       _____       _     _             
+    P^:::^^^^^^P      |__   __|           / ____|    (_)     |  __ \     (_)   | |          
+    J~~^^~~~~~~J         | |_      _____ | |     ___  _ _ __ | |__) |__ _ _  __| | ___ _ __ 
+    B7~!!~~~!~7B         | \ \ /\ / / _ \| |    / _ \| | '_ \|  _  // _` | |/ _` |/ _ \ '__|
+     #5J7777J55          | |\ V  V / (_) | |___| (_) | | | | | | \ \ (_| | | (_| |  __/ |    　
+       &&&&&&&           |_| \_/\_/ \___/ \_____\___/|_|_| |_|_|  \_\__,_|_|\__,_|\___|_|   
+                                            This Software was Premium                                                      
+                                       
+You HWID: [{get_hwid()}]                
+-----------------------""")
+print("[+] Loading....")
+
+root = tk.Tk()
+root.geometry("1280x720")
+root.resizable(0, 0)
+root.title("TwoCoinRaider")
+
+root.mainloop()
