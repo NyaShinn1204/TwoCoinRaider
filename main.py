be50631f8967a10ecbd23a814c09d663b8cd4887
NyaShinn1204 <team.nyarara@gmail.com>
2023-07-28 13:56:12 +0900

    aa

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 import tkinter as tk
 import customtkinter as ctk
+from PIL import Image
 
 def get_hwid():
     cmd = 'wmic csproduct get uuid'
@@ -27,5 +28,15 @@ root = tk.Tk()
 root.geometry("1280x720")
 root.resizable(0, 0)
 root.title("TwoCoinRaider")
+root.configure(bg="#213A3E")
+
+tk.Label(bg="#142326", width=35, height=720).place(x=0,y=0)
+
+ctk.CTkLabel(master=root, bg_color="#142326", text="", image=ctk.CTkImage(Image.open("data/coin.png"),size=(80, 80))).place(x=20,y=20)
+tk.Label(bg="#142326", text="Two Coin", fg="#fff", font=("Roboto", 20)).place(x=100,y=10)
+tk.Label(bg="#142326", text="Raider", fg="#fff", font=("Roboto", 20)).place(x=160,y=40)
+tk.Label(bg="#142326", text="v1.0.0", fg="#F8F8F8", font=("Roboto", 18)).place(x=100,y=70)
+
+ctk.CTkButton(master=root, fg_color="#28464B", hover_color="#2C8C99", text="Joiner / Leaver", width=210, height=35, font=("Roboto", 18, "normal")).place(x=20,y=120)
 
 root.mainloop()
