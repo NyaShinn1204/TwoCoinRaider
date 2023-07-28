d7fad70ef44fa49c1684ebdca35c8273026056d9
NyaShinn1204 <team.nyarara@gmail.com>
2023-07-28 17:17:55 +0900

    y

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ def get_hwid():
     uuid = uuid[pos1:-15]
     return uuid
 
+def clear_frame(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+def set_moduleframe(num1, num2):
+  frame = module_frame = ctk.CTkFrame(root, width=1010, height=700)
+  module_frame.place(x=260, y=10)
+  module_frame.configure(fg_color="#28464B")
+  clear_frame(frame)
+  if num1 == 1:
+    if num2 == 1:
+      print("1-1")
+  if num1 == 2:
+    if num2 == 1:
+      print("2-2")
+
 print(f"""          
        &#BB#&       
      B?^:::^~?B        _______             _____      _       _____       _     _             
@@ -37,6 +53,7 @@ tk.Label(bg="#142326", text="Two Coin", fg="#fff", font=("Roboto", 20)).place(x=
 tk.Label(bg="#142326", text="Raider", fg="#fff", font=("Roboto", 20)).place(x=160,y=40)
 tk.Label(bg="#142326", text="v1.0.0", fg="#F8F8F8", font=("Roboto", 18)).place(x=100,y=70)
 
-ctk.CTkButton(master=root, fg_color="#28464B", hover_color="#2C8C99", text="Joiner / Leaver", width=210, height=35, font=("Roboto", 18, "normal")).place(x=20,y=120)
+ctk.CTkButton(master=root, fg_color="#28464B", hover_color="#2C8C99", text="Joiner / Leaver", width=210, height=35, font=("Roboto", 18, "normal"), command= lambda: set_moduleframe(1, 1)).place(x=20,y=120)
+ctk.CTkButton(master=root, image=ctk.CTkImage(Image.open("data/info.png"),size=(25, 25)), compound="left", fg_color="#28464B", hover_color="#2C8C99", text="About", width=210, height=35, font=("Roboto", 18, "normal"), command= lambda: set_moduleframe(2, 1)).place(x=20,y=660)
 
 root.mainloop()
