719de4d945ccd196c419f2d2d100176a1b523f0f
NyaShinn1204 <team.nyarara@gmail.com>
2023-08-02 10:35:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,30 @@ class SettingVariable:
   nmspamresult_success = 0
   nmspamresult_failed = 0
 
+# value def
+frame = module_frame = ctk.CTkFrame(root, width=990, height=680)
+module_frame.configure(fg_color="#28464B")
+def slider_event01(value):
+  tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=225,y=111)
+def slider_event02(value):
+  tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=605,y=55)
+def slider_event03(value):
+  tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=220,y=173)
+def clear_entry01():
+  joiner_link.delete(0,tk.END)
+def clear_entry02():
+  joiner_serverid.delete(0,tk.END)
+def clear_entry03():
+  leaver_serverid.delete(0,tk.END)
+def clear_entry04():
+  spam_serverid.delete(0,tk.END)
+def clear_entry05():
+  spam_channelid.delete(0,tk.END)
+def clear_entry06():
+  vcspam_serverid.delete(0,tk.END)
+def clear_entry07():
+  vcspam_channelid.delete(0,tk.END) 
+
 def get_hwid():
     cmd = 'wmic csproduct get uuid'
     uuid = str(subprocess.check_output(cmd))
@@ -227,27 +251,6 @@ def set_moduleframe_single(num1, num2, num3):
   module_frame.place(x=270, y=20)
   module_frame.configure(fg_color="#28464B")
   clear_frame(frame)
-  def slider_event01(value):
-    tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=225,y=111)
-  def slider_event02(value):
-    tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=605,y=55)
-  def slider_event03(value):
-    tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=220,y=173)
-  def clear_entry01():
-    joiner_link.delete(0,tk.END)
-  def clear_entry02():
-    joiner_serverid.delete(0,tk.END)
-  def clear_entry03():
-    leaver_serverid.delete(0,tk.END)
-  def clear_entry04():
-    spam_serverid.delete(0,tk.END)
-  def clear_entry05():
-    spam_channelid.delete(0,tk.END)
-  def clear_entry06():
-    vcspam_serverid.delete(0,tk.END)
-  def clear_entry07():
-    vcspam_channelid.delete(0,tk.END)  
-  
   if num1 == 1:
     if num2 == 1:
       if num3 == 1: # Joiner
@@ -342,7 +345,7 @@ def set_moduleframe_single(num1, num2, num3):
         
 def set_moduleframe(num1, num2):
   global joiner_link,joiner_serverid,joiner_button01,leaver_serverid
-  global vcspam_serverid,vcspam_channelid
+  global spam_serverid,spam_channelid,vcspam_serverid,vcspam_channelid
   set_module_collapsing(num1, num2)
   frame = module_frame = ctk.CTkFrame(root, width=990, height=680)
   module_frame.place(x=270, y=20)
@@ -351,33 +354,19 @@ def set_moduleframe(num1, num2):
   if num1 == 1:
     if num2 == 1:
       # Joiner Frame
-      def slider_event01(value):
-        tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=225,y=111)
-      def slider_event02(value):
-        tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=605,y=55)
-      def clear_entry01():
-        joiner_link.delete(0,tk.END)
-      def clear_entry02():
-        joiner_serverid.delete(0,tk.END)
-      def clear_entry03():
-        leaver_serverid.delete(0,tk.END)
-        
       module_setting_frame = ctk.CTkFrame(module_frame, width=350, height=250, border_width=1, border_color="#C0C0C0", fg_color="#28464B")
       module_setting_frame.place(x=20,y=20)
       tk.Label(module_frame, bg="#28464B", fg="#fff", text="Joiner", font=("Roboto", 14)).place(x=35,y=-1)
       joiner_button01 = ctk.CTkCheckBox(module_setting_frame, bg_color="#28464B", text_color="#fff", border_color="#C0C0C0", checkbox_width=20, checkbox_height=20, hover=False, border_width=3, text="Bypass MemberScreen")
       joiner_button01.place(x=5,y=11)
-      
       ctk.CTkButton(module_setting_frame, text="Clear        ", fg_color="#25747D", hover_color="#2C8C99", width=75, height=25, command=clear_entry01).place(x=5,y=40)
       joiner_link = ctk.CTkEntry(module_setting_frame, bg_color="#28464B", fg_color="#275258", border_color="#275258", text_color="#fff", width=150, height=20)
       joiner_link.place(x=85,y=40)  #27
       tk.Label(module_setting_frame, bg="#28464B", fg="#fff", text="Invite Link", font=("Roboto", 12)).place(x=240,y=38)
-      
       ctk.CTkButton(module_setting_frame, text="Clear        ", fg_color="#25747D", hover_color="#2C8C99", width=75, height=25, command=clear_entry02).place(x=5,y=69)
       joiner_serverid = ctk.CTkEntry(module_setting_frame, bg_color="#28464B", fg_color="#275258", border_color="#275258", text_color="#fff", width=150, height=20)
       joiner_serverid.place(x=85,y=69)
       tk.Label(module_setting_frame, bg="#28464B", fg="#fff", text="Server ID", font=("Roboto", 12)).place(x=240,y=67)
-      
       ctk.CTkSlider(module_setting_frame, from_=0.1, to=3.0, variable=Setting.delay01, command=slider_event01).place(x=5,y=96)
       tk.Label(module_setting_frame, bg="#28464B", fg="#fff", text=round(Setting.delay01.get(),1), font=("Roboto", 12)).place(x=205,y=91)
       tk.Label(module_setting_frame, bg="#28464B", fg="#fff", text="Delay", font=("Roboto", 12)).place(x=240,y=91)
@@ -413,17 +402,6 @@ def set_moduleframe(num1, num2):
     
     if num2 == 2:
       # Spammer
-      def clear_entry04():
-        spam_serverid.delete(0,tk.END)
-      def clear_entry05():
-        spam_channelid.delete(0,tk.END)
-      def clear_entry06():
-        vcspam_serverid.delete(0,tk.END)
-      def clear_entry07():
-        vcspam_channelid.delete(0,tk.END)
-      def slider_event03(value):
-        tk.Label(module_frame, bg="#28464B", fg="#fff", text=round(value,1), font=("Roboto", 12)).place(x=220,y=173)
-      
       module_setting_frame = ctk.CTkFrame(module_frame, width=350, height=250, border_width=1, border_color="#C0C0C0", fg_color="#28464B")
       module_setting_frame.place(x=20,y=20)
       tk.Label(module_frame, bg="#28464B", fg="#fff", text="Spammer", font=("Roboto", 14)).place(x=35,y=-1)
