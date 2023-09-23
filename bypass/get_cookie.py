e1145b983df971430c1fac5ed285fe6a4f4077a1
NyaShinn1204 <team.nyarara@gmail.com>
2023-09-23 20:11:27 +0900

    fix get cookie

--- bypass/get_cookie.py
+++ bypass/get_cookie.py
@@ -2,5 +2,5 @@ import requests
 
 def get_cookie():
     session = requests.Session()
-    found_cookies = session.cookies.get_dict()
-    return f'__dcfduid={found_cookies["__dcfduid"]}; __sdcfduid={found_cookies["__sdcfduid"]}; locale=ja-JP'
+    response = session.get("https://discord.com/api/v9/experiments").cookies
+    return f'__dcfduid={response["__dcfduid"]}; __sdcfduid={response["__sdcfduid"]}; locale=ja-JP'
