f69013c2d3171f5df2be42327094dbb9f5688ce2
NyaShinn1204 <team.nyarara@gmail.com>
2023-12-05 18:49:36 +0900

    Rewrite Onliner

--- module/tkonliner.py
+++ module/tkonliner.py
@@ -35,137 +35,33 @@ def online_thread(token, status, type, randomse):
     try:
         if randomse == True:
             type = random.choice(['Playing', 'Streaming', 'Watching', 'Listening', ''])
-            status = ['online', 'dnd', 'idle']
-            status = random.choice(status)
-            ws = websocket.WebSocket()
-            ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
-            hello = json.loads(ws.recv())
-            heartbeat_interval = hello['d']['heartbeat_interval']
-            if type == "Playing":
-                game = "Minecraft"
-                gamejson = {
-                    "name": game,
-                    "type": 0
-                }
-            elif type == 'Streaming':
-                gamejson = {
-                    "name": game,
-                    "type": 1,
-                    "url": stream_text
-                }
-            elif type == "Listening":
-                game = random.choice(["Spotify", "Apple Music", "YouTube", "SoundCloud"])
-                gamejson = {
-                    "name": game,
-                    "type": 2
-                }
-            elif type == "Watching":
-                game = random.choice(["YouTube", "Twitch"])
-                gamejson = {
-                    "name": game,
-                    "type": 3
-                }
-            else:
-                gamejson = {
-                    "name": game,
-                    "type": 0
-                }
-            auth = {
-                "op": 2,
-                "d": {
-                    "token": token,
-                    "properties": {
-                        "$os": sys.platform,
-                        "$browser": "RTB",
-                        "$device": f"{sys.platform} Device"
-                    },
-                    "presence": {
-                        "game": gamejson,
-                        "status": status,
-                        "since": 0,
-                        "afk": False
-                    }
-                },
-                "s": None,
-                "t": None
-            }
-            ws.send(json.dumps(auth))
-            ack = {
-                "op": 1,
-                "d": None
-            }
-            while True:
-                time.sleep(heartbeat_interval / 1000)
-                try:
-                    print(f"[+] Success Online: {extract_token}")
-                    ws.send(json.dumps(ack))
-                except Exception as e:
-                    print(f"[-] Failed Online: {extract_token}")
-                    break
+            status = random.choice(['online', 'dnd', 'idle'])
+        ws = websocket.WebSocket()
+        ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
+        hello = json.loads(ws.recv())
+        heartbeat_interval = hello['d']['heartbeat_interval']
+        if type == "Playing":
+            gamejson = {"name": game,"type": 0}
+        elif type == 'Streaming':
+            gamejson = {"name": game,"type": 1,"url": stream_text}
+        elif type == "Listening":
+            gamejson = {"name": game,"type": 2}
+        elif type == "Watching":
+            gamejson = {"name": game,"type": 3}
         else:
-            ws = websocket.WebSocket()
-            ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
-            hello = json.loads(ws.recv())
-            heartbeat_interval = hello['d']['heartbeat_interval']
-            if type == "Playing":
-                gamejson = {
-                    "name": game,
-                    "type": 0
-                }
-            elif type == 'Streaming':
-                gamejson = {
-                    "name": game,
-                    "type": 1,
-                    "url": stream_text
-                }
-            elif type == "Listening":
-                gamejson = {
-                    "name": game,
-                    "type": 2
-                }
-            elif type == "Watching":
-                gamejson = {
-                    "name": game,
-                    "type": 3
-                }
-            else:
-                gamejson = {
-                    "name": game,
-                    "type": 0
-                }
-            auth = {
-                "op": 2,
-                "d": {
-                    "token": token,
-                    "properties": {
-                        "$os": sys.platform,
-                        "$browser": "RTB",
-                        "$device": f"{sys.platform} Device"
-                    },
-                    "presence": {
-                        "game": gamejson,
-                        "status": status,
-                        "since": 0,
-                        "afk": False
-                    }
-                },
-                "s": None,
-                "t": None
-            }
-            ws.send(json.dumps(auth))
-            ack = {
-                "op": 1,
-                "d": None
-            }
-            while status is True:
-                if status == False:
-                    break
-                time.sleep(heartbeat_interval / 1000)
-                try:
-                    print(f"[+] Success Online: {extract_token}")
-                    ws.send(json.dumps(ack))
-                except Exception as e:
-                    print(f"[-] Failed Online: {extract_token}")
-                    break
+            gamejson = {"name": game,"type": 0}
+        auth = {"op": 2,"d": {"token": token,"properties": {"$os": sys.platform,"$browser": "RTB","$device": f"{sys.platform} Device"},"presence": {"game": gamejson,"status": status,"since": 0,"afk": False}},"s": None,"t": None}
+        ws.send(json.dumps(auth))
+        ack = {"op": 1,"d": None}
+        while status is True:
+            if status == False:
+                break
+            time.sleep(heartbeat_interval / 1000)
+            try:
+                print(f"[+] Success Online: {extract_token}")
+                ws.send(json.dumps(ack))
+            except Exception as e:
+                print(f"[-] Failed Online: {extract_token}")
+                break
     except:
         pass
