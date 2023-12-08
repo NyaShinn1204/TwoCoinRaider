3da6f133cdaff7e491f9c506a897da4941ba7255
NyaShinn1204 <team.nyarara@gmail.com>
2023-12-08 17:27:40 +0900

    Fix Joiner Str Error

--- module/joiner.py
+++ module/joiner.py
@@ -73,7 +73,7 @@ def joiner_thread(token, serverid, invitelink, memberscreen, module_status, answ
         joinreq = session.post(f"https://discord.com/api/v9/invites/{invitelink}", headers=headers, json={})
         if joinreq.status_code == 400:
             if bypasscaptcha == True:
-                printl("info", f"{pretty}Solving Captcha {gray} | " + Fore.GREEN + extract_token + Fore.RESET)
+                printl("info", f"{pretty}Solving Captcha{gray} | " + Fore.GREEN + extract_token + Fore.RESET)
                 payload = {
                     "captcha_key": solver.bypass_captcha(answers, token, "https://discord.com", joinreq.json()['captcha_sitekey'], apis)
                 }
@@ -81,33 +81,33 @@ def joiner_thread(token, serverid, invitelink, memberscreen, module_status, answ
                 if newresponse.status_code == 200:
                     if "captcha_key" not in newresponse.json():
                         if "You need to verify your account in order to perform this action." in newresponse.json():
-                            printl("error", f"{pretty}認証が必要です {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET)
+                            printl("error", f"{pretty}認証が必要です {gray}| " + Fore.CYAN + extract_token + Fore.RESET)
                             module_status(1, 1, 2)
-                        printl("info", f"{pretty}Successfully Token Join {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET)
+                        printl("info", f"{pretty}Successfully Token Join {gray}| " + Fore.CYAN + extract_token + Fore.RESET)
                         if delete_joinms == True:
-                            printl("info", f"{pretty}Deleting Join Message {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET)
+                            printl("info", f"{pretty}Deleting Join Message {gray}| " + Fore.CYAN + extract_token + Fore.RESET)
                             delete_join_msg(token, join_channelid)
                         module_status(1, 1, 1)
                     if memberscreen == True:
                         member_screen_bypass(token, joinreq.json(), joinreq.json()["guild"]["id"])
                 else:
-                    printl("error", f"{pretty}Failed Captcha Bypass {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET+ " | " + newresponse.text)
+                    printl("error", f"{pretty}Failed Captcha Bypass {gray}| " + Fore.CYAN + extract_token + Fore.RESET+ " | " + newresponse.text)
             else:
                 if "captcha_key" in joinreq.json():
-                    printl("error", f"{pretty}Failed Token Join (Captcha Wrong) {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET)
+                    printl("error", f"{pretty}Failed Token Join (Captcha Wrong) {gray}| " + Fore.CYAN + extract_token + Fore.RESET)
                     print(joinreq.json())
                     module_status(1, 1, 2)
         if joinreq.status_code == 200:
             if "captcha_key" not in joinreq.json():
                 if joinreq.json().get("message") == "The user is banned from this guild.":
-                    printl("error", f"{pretty}サーバーからBANされています {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET)
+                    printl("error", f"{pretty}サーバーからBANされています {gray}| " + Fore.CYAN + extract_token + Fore.RESET)
                     module_status(1, 1, 2)
                 if "You need to verify your account in order to perform this action." in joinreq.json():
-                    printl("error", f"{pretty}認証が必要です {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET)
+                    printl("error", f"{pretty}認証が必要です {gray}| " + Fore.CYAN + extract_token + Fore.RESET)
                     module_status(1, 1, 2)
-                printl("info", f"{pretty}Successfully Token Join {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET)
+                printl("info", f"{pretty}Successfully Token Join {gray}| " + Fore.CYAN + extract_token + Fore.RESET)
                 if delete_joinms == True:
-                    printl("info", f"{pretty}Deleting Join Message {gray}| " + {Fore.CYAN} + extract_token + Fore.RESET)
+                    printl("info", f"{pretty}Deleting Join Message {gray}| " + Fore.CYAN + extract_token + Fore.RESET)
                     delete_joinms(token, headers, join_channelid)
                 module_status(1, 1, 1)
             if memberscreen == True:
