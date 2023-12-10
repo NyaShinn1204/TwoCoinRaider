bbe414d4ae36cf4c9a65ce6a47d77bf43940367a
NyaShinn1204 <team.nyarara@gmail.com>
2023-12-10 18:32:04 +0900

    Add Disable Resize
    Adding theme

--- test/new-gui.py
+++ test/new-gui.py
@@ -29,13 +29,13 @@ version = "v1.0.3β"
 theme = "twocoin"
 
 if theme == "akebi":
-    c1 = ""
+    c1 = "#040f24"
     c2 = "#020b1f"
     c3 = "#0a2b63"
     c4 = "#020b1f"
     c5 = "#00bbe3"
 if theme == "twocoin":
-    c1 = ""
+    c1 = "#28464B"
     c2 = "#213A3E"
     c3 = "#00484C"
     c4 = "#142326"
@@ -43,18 +43,44 @@ if theme == "twocoin":
 
 root = tk.Tk()
 root.geometry("1280x720")
+root.resizable(0, 0)
 root.title("TwoCoinRaider | "+version)
 root.iconbitmap(default="data/favicon.ico")
 root.configure(bg=c2)
 
+def get_filename():
+  return os.path.basename(__file__)
 
-tk.Label(bg=c4, width=32, height=720).place(x=0,y=0)
-tk.Label(bg=c4, text="TWOCOIN RAIDER", fg="#fff", font=("Carlito", 20, "bold")).place(x=13,y=25)
+def printl(num, data):
+  if num == "error":
+    print(f"[{Fore.LIGHTRED_EX}Error{Fore.RESET}] [{get_filename()}] " + data)
+  if num == "debug":
+    print(f"[{Fore.LIGHTCYAN_EX}Debug{Fore.RESET}] [{get_filename()}] " + data)
+  if num == "info":
+    print(f"[{Fore.LIGHTGREEN_EX}Info{Fore.RESET}] [{get_filename()}] " + data)
+
+def clear_frame(frame):
+  for widget in frame.winfo_children():
+    widget.destroy()
+  frame.pack_forget()
+
+def set_moduleframe_scroll(num1, num2):
+    frame_scroll = module_frame = ctk.CTkScrollableFrame(root, width=970, height=630)
+    module_frame.place(x=245, y=70)
+    module_frame.configure(fg_color=c2)
+    clear_frame(frame_scroll)
+    if num1 == 1:
+        if num2 == 1:
+            printl("debug", "Open Join Leave Tab")
+
+
+tk.Label(root, bg=c4, width=32, height=720).place(x=0,y=0)
+tk.Label(root, bg=c4, text="TWOCOIN RAIDER", fg="#fff", font=("Carlito", 20, "bold")).place(x=13,y=25)
 
 modulelist = ctk.CTkFrame(master=root, width=230, height=720, corner_radius=0, fg_color=c4)
 modulelist.place(x=0,y=100)
 tk.Canvas(bg=c3, highlightthickness=0, height=2080, width=4).place(x=230, y=10)
-ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color=c4, hover_color=c5, corner_radius=0, text="Joiner / Leaver", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w").place(x=20,y=12)
+ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color=c4, hover_color=c5, corner_radius=0, text="Joiner / Leaver", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w", command= lambda: set_moduleframe_scroll(1, 1)).place(x=20,y=12)
 ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color=c4, hover_color=c5, corner_radius=0, text="Spammer", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w").place(x=20,y=57)
 ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color=c4, hover_color=c5, corner_radius=0, text="Soon", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w").place(x=20,y=102)
 ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color=c4, hover_color=c5, corner_radius=0, text="Soon", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w").place(x=20,y=148)
@@ -62,5 +88,10 @@ ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.
 ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color=c4, hover_color=c5, corner_radius=0, text="Soon", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w").place(x=20,y=240)
 ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color=c4, hover_color=c5, corner_radius=0, text="Soon", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w").place(x=20,y=286)
 ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color=c4, hover_color=c5, corner_radius=0, text="Soon", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w").place(x=20,y=332)
+
+credit_frame = ctk.CTkFrame(root, width=990, height=50, fg_color=c1)
+credit_frame.place(x=245, y=10)
+ctk.CTkLabel(master=credit_frame, image=ctk.CTkImage(Image.open("data/user.png"),size=(15, 15)), compound="right", fg_color=c1, text_color="#fff", corner_radius=0, text=os.getlogin(), width=195, height=40, font=("Roboto", 16, "bold"), anchor="w").place(x=900,y=10)
+
 
 root.mainloop()
