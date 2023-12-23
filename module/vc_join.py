c9624c67bb3b585864267190cd1a3741a93cd7ab
NyaShinn1204 <team.nyarara@gmail.com>
2023-12-23 11:41:35 +0900

    Remove VC Leaver Fix VC Joiner

--- module/vc_join.py
+++ module/vc_join.py
@@ -7,7 +7,17 @@ mic_mute = True
 speaker_mute = True
 keep = True
 
-def start(delay, tokens, module_status, serverid, channelid, type):
+status = True
+
+def status():
+    global status
+    return status
+
+def stop():
+    global status
+    status = False
+
+def start(delay, tokens, module_status, serverid, channelid):
     global status
     status = True
     for token in tokens:
@@ -18,7 +28,7 @@ def start(delay, tokens, module_status, serverid, channelid, type):
         else:
             break
 
-def join_vc(token, module_status, serverid, channelid):
+def join_vc(token, module_status, serverid, channelid, video, mic_mute, speaker_mute, keep):
     ws = websocket.WebSocket()
     ws.connect("wss://gateway.discord.gg/?encoding=json&v=9&compress=zlib-stream")
     ws.send(f'{{"op":2,"d":{{"token":"{token}","capabilities":16381,"properties":{{"os":"Windows","browser":"Chrome","device":"","system_locale":"ja","os_version":"10","referrer":"","referring_domain":"","referrer_current":"","referring_domain_current":"","release_channel":"stable","client_event_source":null}},"presence":{{"status":"online","since":0,"activities":[],"afk":false}},"compress":false,"client_state":{{"guild_versions":{{}},"highest_last_message_id":"0","read_state_version":0,"user_guild_settings_version":-1,"user_settings_version":-1,"private_channels_version":"0","api_code_version":0}}}}}}')
